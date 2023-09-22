3f83668a1f2f3c1e370f46782f5a5c76a70a4feb
sherryy14 <muhammadshahryar199765@gmail.com>
2023-09-23 02:33:19 +0500

    Syndash

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -49,7 +50,41 @@ class ViewController extends Controller
         $data = compact('categories');
         return view('add-product')->with($data);
     }
-    public function addproduct(){
+    public function addproduct(Request $request){
+
+        $rules = [
+            'title' => 'required|string|max:255',
+            'category' => 'required|exists:categories,cat_id',
+            'weight' => 'required|numeric',
+            'unit' => 'required|integer|min:1',
+            'file1' => 'required|image|mimes:jpeg,png,gif',
+            'file2' => 'required|image|mimes:jpeg,png,gif',
+            'file3' => 'required|image|mimes:jpeg,png,gif',
+            'file4' => 'required|image|mimes:jpeg,png,gif',
+            'desc' => 'required|string',
+            'availability' => 'boolean',
+            'code' => 'required|string|max:255',
+            'status' => 'required|in:Active,Disable',
+            'price' => 'required|numeric',
+        ];
+    
+        // Define custom error messages
+        $messages = [
+            'category.exists' => 'Selected category does not exist.',
+            'file1.*' => 'File 1 must be a valid image (jpeg, png, gif).',
+            'file2.*' => 'File 2 must be a valid image (jpeg, png, gif).',
+            'file3.*' => 'File 3 must be a valid image (jpeg, png, gif).',
+            'file4.*' => 'File 4 must be a valid image (jpeg, png, gif).',
+        ];
+    
+        // Validate the request data
+        $validator = Validator::make($request->all(), $rules, $messages);
+    
+        // Check if validation fails
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
 
         return redirect('/product');
     }
--- resources/views/add-product.blade.php
+++ resources/views/add-product.blade.php
@@ -17,6 +17,7 @@
              
 
                 <form method="post" enctype="multipart/form-data">
+                    @csrf
                     <div class="row">
                       <div class="col-lg-8 col-12">
                         <!-- card -->
@@ -28,12 +29,17 @@
                               <!-- input -->
                               <div class="mb-3 col-lg-6">
                                 <label class="form-label">Title</label>
-                                <input type="text" name="title" class="form-control" placeholder="Product Name" required>
+                                <input type="text" name="title" class="form-control" placeholder="Product Name" >
+                                <span class="text-danger">
+                                    @error('title')
+                                        {{$message}}
+                                    @enderror
+                                </span>
                               </div>
                               <!-- input -->
                               <div class="mb-3 col-lg-6">
                                                         <label class="form-label">Parent Category</label>
-                                                        <select class="form-select" name="category" required>
+                                                        <select class="form-select" name="category" >
                                                             <option selected disabled>Category Name</option>
          
                                                             @foreach ($categories as $category)
@@ -41,16 +47,31 @@
                                                             @endforeach
                                                             
                                                         </select>
+                                                        <span class="text-danger">
+                                                            @error('category')
+                                                                {{$message}}
+                                                            @enderror
+                                                        </span>
                                                     </div>
                               <!-- input -->
                               <div class="mb-3 col-lg-6">
                                 <label class="form-label">Weight</label>
-                                <input type="number" name="weight" class="form-control" placeholder="Weight" required>
+                                <input type="number" name="weight" class="form-control" placeholder="Weight" >
+                                <span class="text-danger">
+                                    @error('weight')
+                                        {{$message}}
+                                    @enderror
+                                </span>
                               </div>
                               <!-- input -->
                               <div class="mb-3 col-lg-6">
                                 <label class="form-label">Units</label>
-                               <input type="number" name="unit" min="1" class="form-control" placeholder="Unit" required>
+                               <input type="number" name="unit" min="1" class="form-control" placeholder="Unit" >
+                               <span class="text-danger">
+                                @error('unit')
+                                    {{$message}}
+                                @enderror
+                            </span>
                               </div>
                               <div>
                                 <div class="mb-3 col-lg-12 mt-5">
@@ -60,12 +81,32 @@
                                   <!-- input -->
                                   
                                     <div class="fallback">
-                                      <input name="file1" type="file" class="form-control my-2" accept="image/png, image/gif, image/jpeg" required> 
-                                      <input name="file2" type="file" class="form-control my-2" accept="image/png, image/gif, image/jpeg" required> 
-                                      <input name="file3" type="file" class="form-control my-2" accept="image/png, image/gif, image/jpeg" required>
-                                      <input name="file4" type="file" class="form-control my-2" accept="image/png, image/gif, image/jpeg" required>
+                                      <input name="file1" type="file" class="form-control my-2" accept="image/png, image/gif, image/jpeg" >
+                                      <span class="text-danger">
+                                        @error('file1')
+                                            {{$message}}
+                                        @enderror
+                                    </span> 
+                                      <input name="file2" type="file" class="form-control my-2" accept="image/png, image/gif, image/jpeg" > 
+                                      <span class="text-danger">
+                                        @error('file2')
+                                            {{$message}}
+                                        @enderror
+                                    </span>
+                                      <input name="file3" type="file" class="form-control my-2" accept="image/png, image/gif, image/jpeg" >
+                                      <span class="text-danger">
+                                        @error('file3')
+                                            {{$message}}
+                                        @enderror
+                                    </span>
+                                      <input name="file4" type="file" class="form-control my-2" accept="image/png, image/gif, image/jpeg" >
+                                      <span class="text-danger">
+                                        @error('file4')
+                                            {{$message}}
+                                        @enderror
+                                    </span>
                                     </div>
-                                 
+                                    
                                 </div>
                               </div>
                               <!-- input -->
@@ -73,7 +114,12 @@
                                 <h4 class="mb-3 h5">Product Descriptions</h4>
                                 <!-- <div class="py-8" id="edi
                                 tor"></div> -->
-                                <textarea name="desc" placeholder="Write description" cols="30" rows="5" required class="form-control"></textarea> 
+                                <textarea name="desc" placeholder="Write description" cols="30" rows="5"  class="form-control"></textarea> 
+                                <span class="text-danger">
+                                    @error('desc')
+                                        {{$message}}
+                                    @enderror
+                                </span>
                               </div>
                             </div>
                           </div>
@@ -94,7 +140,12 @@
                             <div>
                               <div class="mb-3">
                                 <label class="form-label">Product Code</label>
-                                <input type="text" required name="code" class="form-control" placeholder="Enter Product Title">
+                                <input type="text"  name="code" class="form-control" placeholder="Enter Product Title">
+                                <span class="text-danger">
+                                    @error('code')
+                                        {{$message}}
+                                    @enderror
+                                </span>
                               </div>
                              
                               <!-- input -->
@@ -126,7 +177,12 @@
                             <!-- input -->
                             <div class="mb-3">
                               <label class="form-label">Price</label>
-                              <input type="number" name="price" class="form-control" placeholder="$0.00 " required>
+                              <input type="number" name="price" class="form-control" placeholder="$0.00 " >
+                              <span class="text-danger">
+                                @error('price')
+                                    {{$message}}
+                                @enderror
+                            </span>
                             </div>
                          
             
